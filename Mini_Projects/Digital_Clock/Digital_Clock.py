66c39988c88dee4b057eb422924a3d2e2b54ccf0
manasiii <110840477+mannasiii@users.noreply.github.com>
2023-03-04 22:33:08 +0530

    Create Digital_Clock.py

--- Mini_Projects/Digital_Clock/Digital_Clock.py
+++ Mini_Projects/Digital_Clock/Digital_Clock.py
@@ -9,6 +9,7 @@ root.title("Clock")
 def present_time():
     display_time = time.strftime("%d-%m-%y\n%I: %M : %S %p")
 
+
     Digi_Clock.config(text= display_time)
     Digi_Clock.after(150, present_time)
 Digi_Clock = Label(root, font=("Comic Sans", 55), bg="Black", fg="White")
